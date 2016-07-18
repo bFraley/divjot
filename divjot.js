1eac45013624a0359c302aa6763b1b32c31f4091
bFraley <bFraley@users.noreply.github.com>
2016-07-18 18:14:58 -0400

    bring in old jaunt files for revamp

--- divjot.js
+++ divjot.js
@@ -1,12 +1,13 @@
+// Copyright by Brett Fraley 2016
 
-var jauntwrapper = document.getElementById('jaunt-wrapper');
+var divjot_wrapper = document.getElementById('divjot-wrapper');
 
 // Editor textarea elements
-var jaunthtml = document.getElementById('jaunt-html');
-var jauntcss = document.getElementById('jaunt-css');
-var jauntjs = document.getElementById('jaunt-js');
-var usermarkup = document.getElementById('jaunt-markup');
-var userstyle = document.getElementById('jaunt-style');
+var divjot_html = document.getElementById('divjot-html');
+var divjot_css = document.getElementById('divjot-css');
+var divjot_js = document.getElementById('divjot-js');
+var usermarkup = document.getElementById('divjot-markup');
+var userstyle = document.getElementById('divjot-style');
 
 // Editor control buttons
 var controls = {
@@ -24,9 +25,9 @@ var controls = {
 /* Functions */
 
 // Send user source to DOM.
-function markup_out() { usermarkup.innerHTML=(jaunthtml.value); }
-function style_out() { userstyle.innerHTML=(jauntcss.value); }
-function js_out() { return eval(jauntjs.value); }
+function markup_out() { usermarkup.innerHTML=(divjot_html.value); }
+function style_out() { userstyle.innerHTML=(divjot_css.value); }
+function js_out() { return eval(divjot_js.value); }
 
 // Toggle 'show/hide' editors.
 function toggle(el) {
@@ -38,26 +39,26 @@ function toggle(el) {
 
 function font_smaller() {
     controls.fsize -= 2;
-    jaunthtml.style.fontSize = controls.fsize + "pt";
-    jauntcss.style.fontSize = controls.fsize + "pt";
-    jauntjs.style.fontSize = controls.fsize + "pt";
+    divjot_html.style.fontSize = controls.fsize + "pt";
+    divjot_css.style.fontSize = controls.fsize + "pt";
+    divjot_js.style.fontSize = controls.fsize + "pt";
 }
 
 function font_bigger() {
     controls.fsize += 2;
-    jaunthtml.style.fontSize = controls.fsize + "pt";
-    jauntcss.style.fontSize = controls.fsize + "pt";
-    jauntjs.style.fontSize = controls.fsize + "pt";
+    divjot_html.style.fontSize = controls.fsize + "pt";
+    divjot_css.style.fontSize = controls.fsize + "pt";
+    divjot_js.style.fontSize = controls.fsize + "pt";
 }
 
 function dark() {
-    jauntwrapper.style.backgroundColor = "#333";
+    divjot_wrapper.style.backgroundColor = "#333";
     controls.dark.style.display = "none";
     controls.light.style.display = "inline";
 }
 
 function light() {
-    jauntwrapper.style.backgroundColor = "#FFF";
+    divjot_wrapper.style.backgroundColor = "#FFF";
     controls.light.style.display = "none";
     controls.dark.style.display = "inline";
 }
@@ -65,16 +66,16 @@ function light() {
 /* Events */
 
 // User code I/O event triggers. 
-jaunthtml.addEventListener('keyup' || 'keypress', markup_out, false);
+divjot_html.addEventListener('keyup' || 'keypress', markup_out, false);
 
-jauntcss.addEventListener('keyup' || 'keypress', style_out, false);
+divjot_css.addEventListener('keyup' || 'keypress', style_out, false);
 
-jauntjs.addEventListener('keypress', function(key) {
+divjot_js.addEventListener('keypress', function(key) {
     if (key.keyCode === 13)    js_out();    }, false);
 
 // Control button event triggers.
 controls.opacity.addEventListener('input', function() {
-    jauntwrapper.style.opacity = controls.opacity.value; }, false);
+    divjot_wrapper.style.opacity = controls.opacity.value; }, false);
 
 controls.fsmaller.addEventListener('click', font_smaller, false);
 
@@ -84,14 +85,9 @@ controls.dark.addEventListener('click', dark, false);
 
 controls.light.addEventListener('click', light, false);
 
-controls.html.addEventListener('click', function() { toggle(jaunthtml); }, false);
+controls.html.addEventListener('click', function() { toggle(divjot_html); }, false);
 
-controls.css.addEventListener('click', function() { toggle(jauntcss); }, false);
+controls.css.addEventListener('click', function() { toggle(divjot_css); }, false);
 
-controls.js.addEventListener('click', function() { toggle(jauntjs); }, false); 
-
-
-// code output
-
-
+controls.js.addEventListener('click', function() { toggle(divjot_js); }, false); 
 
